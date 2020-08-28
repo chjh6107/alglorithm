a41a8b4e89753fe3fbcbfee1e0ad9b80bd7bb42d
chjh6107 <chjh6107@gmail.com>
2020-08-28 21:54:18 +0900

    for

---
+++ 단계별로풀어보기/for/11022.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int c=0,T,A,B;
+    scanf("%d",&T);
+    for(int i=0;i<T;i++){
+        c++;
+        scanf("%d %d",&A,&B);
+        printf("Case #%d: %d + %d = %d\n",c,A,B,A+B);
+    }
+}
