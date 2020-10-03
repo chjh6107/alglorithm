7b5165101f52865924730b122b555d2061e6a8c6
chjh6107 <chjh6107@gmail.com>
2020-10-03 16:29:21 +0900

    19952인성 문제 있어??

---
+++ 19952.c
@@ -0,0 +1,18 @@
+// 인성 문제 있어??
+#include <stdio.h>
+
+typedef struct{
+    int len;
+}Map;
+
+int main(){
+    /* H-세로 W-가로 O-장애물개수 -F현재힘 */
+    int T,H,W,O,F,Xs,Ys,Xe,Ye;
+    for(int i=0; i<T;i++){
+
+    }
+    // Map map[3][4];
+
+    // map[0][0].len=21;
+    // printf("%d\n",map[0][0].len);
+}
