f5494093c887c3f6921fe9de127d40bad570963b
chjh6107 <chjh6107@gmail.com>
2020-10-16 03:06:19 +0900

    거스름돈

---
+++ 5585.c
@@ -0,0 +1,14 @@
+// 거스름돈
+#include <stdio.h>
+
+int main(){
+    int n,count=0;
+    int a[6]={500,100,50,10,5,1};
+    scanf("%d",&n);
+    n=1000-n;
+    for(int i=0;i<6;i++){
+        count+=n/a[i];
+        n%=a[i];
+    }
+    printf("%d\n",count);
+}
