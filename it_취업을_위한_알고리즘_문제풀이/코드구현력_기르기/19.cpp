c4d47c5c8c64adcb446607e58a209e66e03e766f
chjh6107 <chjh6107@gmail.com>
2021-01-18 01:37:51 +0900

    분노 유발자

---
+++ it_취업을_위한_알고리즘_문제풀이/코드구현력_기르기/19.cpp
@@ -0,0 +1,21 @@
+// 19. 분노 유발자
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, arr[100] = { 0, }, cnt = 0, flag;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	for (int i = 0; i < n - 1; i++) {
+		flag = 0;
+		for (int j = i + 1; j < n; j++) {
+			if (arr[i] < arr[j]) {
+				flag = 1;
+				break;
+			}
+		}
+		if (!flag)cnt++;
+	}
+	cout << cnt;
+}
