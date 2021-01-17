92d9091774db4e17e85b6d282b78cfefd528454d
chjh6107 <chjh6107@gmail.com>
2021-01-17 22:26:54 +0900

    소수의 개수

--- it_취업을_위한_알고리즘_문제풀이/코드구현력_기르기/15.cpp
+++ it_취업을_위한_알고리즘_문제풀이/코드구현력_기르기/15.cpp
@@ -2,23 +2,6 @@
 #include <iostream>
 using namespace std;
 
-int reverse(int x) {
-	int ret = 0;
-	while (x) {
-		ret = x%10 + ret*10;
-		x /= 10;
-	}
-	return ret;
-}
-
-bool isPrime(int x) {
-	if (x == 1) return false;
-	for (int i = 2; i < x; i++) {
-		if (!(x%i)) return false;
-	}
-	return true;
-}
-
 int main() {
 	int n, num, flag = 1, cnt = 0;;
 	cin >> n;
