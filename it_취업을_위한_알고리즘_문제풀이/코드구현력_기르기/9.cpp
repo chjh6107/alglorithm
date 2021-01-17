17db285cc75d430e3aff94b434555d75c994c3ad
chjh6107 <chjh6107@gmail.com>
2021-01-17 16:22:16 +0900

    모두의 약수(modified)

--- it_취업을_위한_알고리즘_문제풀이/코드구현력_기르기/9.cpp
+++ it_취업을_위한_알고리즘_문제풀이/코드구현력_기르기/9.cpp
@@ -1,10 +1,10 @@
 // 9. 모두의 약수
 #include <iostream>
+#include <vector>
 using namespace std;
-int n, arr[50001];
-int main(){
-	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
+int main(){
+	int n, arr[50001];
 	cin>>n;
 	for(int i=1;i<=n;i++){
 		for(int j=i;j<=n;j=j+i) arr[j]++;
