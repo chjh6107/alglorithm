403f054c20316d23737577dd8980350c104c70ee
chjh6107 <chjh6107@gmail.com>
2021-01-20 23:09:15 +0900

    버블정렬(modified)

--- it_취업을_위한_알고리즘_문제풀이/정렬_이분탐색-결정알고리즘_투포인트_알고리즘_스택/34.cpp
+++ it_취업을_위한_알고리즘_문제풀이/정렬_이분탐색-결정알고리즘_투포인트_알고리즘_스택/34.cpp
@@ -11,9 +11,9 @@ int main() {
 
 	for (int i = 0; i < n; i++) cin >> a[i];
 
-	for (int i = n; i >= 0; i--) {
-		for (int j = 1; j < i; j++) {
-			if (a[j-1] > a[j]) swap(a[j-1], a[j]);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n-i-1 ; j++) {
+			if (a[j] > a[j+1]) swap(a[j], a[j+1]);
 		}
 	}
 	for (int val : a)cout << val << " ";
