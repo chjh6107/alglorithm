5c1abf13a127316ba2cc82ed0d52c343d5db8f24
chjh6107 <chjh6107@gmail.com>
2021-01-27 19:56:58 +0900

    영지(territory) 선택 : (small)

---
+++ it_취업을_위한_알고리즘_문제풀이/정렬_이분탐색-결정알고리즘_투포인트_알고리즘_스택/50.cpp
@@ -0,0 +1,28 @@
+// 50. 영지(territory) 선택 : (small)
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+int main(){
+    int H,W, max=0,tH, tW,acc;
+    scanf("%d %d", &H, &W);
+    vector<vector<int>> a(H, vector<int>(W));
+    for(int i=0;i<H; i++)
+        for(int j=0;j<W;j++)
+            scanf("%d",&a[i][j]);
+    
+    scanf("%d %d",&tH, &tW);
+    // printf("%d\n",H-tH+1);
+    for(int i=0;i<H-tH+1;i++){
+        for(int j=0;j<W-tW+1;j++){
+            acc=0;
+            for(int ti=i;ti<i+tH;ti++){
+                for(int tj=j;tj<j+tW;tj++){
+                    acc+=a[ti][tj];
+                }
+            }
+            max=acc>max?acc:max;
+        }
+    }
+    printf("%d",max);
+}
