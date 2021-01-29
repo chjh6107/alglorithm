3f43d50e52a57446229aee27fe1909ba71fc56c0
chjh6107 <chjh6107@gmail.com>
2021-01-29 23:24:25 +0900

    기차운행(stack 응용)

---
+++ it_취업을_위한_알고리즘_문제풀이/정렬_이분탐색-결정알고리즘_투포인트_알고리즘_스택/55.cpp
@@ -0,0 +1,26 @@
+// 55. 기차운행(stack 응용)
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n,train, target=1;
+    vector<char> v;
+    stack<int> s;
+    // scanf("%d",&n);
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        scanf("%d",&train);
+        s.push(train);
+        v.push_back('P');
+        while(!s.empty() && s.top()==target){
+            s.pop();
+            v.push_back('O');
+            target++;
+        }
+    }
+    if(s.empty()) for(char c:v) cout<<c;
+    else cout<<"impossible";
+}
