88addd587ed105f8430a223ab69735ad9e0e5bf6
chjh6107 <chjh6107@gmail.com>
2021-01-30 21:15:00 +0900

    특정 수 만들기(DFS : MS 인터뷰)

--- it_취업을_위한_알고리즘_문제풀이/재귀_DFS_BFS/61.cpp
+++ it_취업을_위한_알고리즘_문제풀이/재귀_DFS_BFS/61.cpp
@@ -4,25 +4,25 @@
 using namespace std;
 int n,m,a[11], ch[11],acc=0;
 
-void calDfs(vector<int> A, int size, int L, int sum){
-    if(L>size){
+void calDfs(vector<int> A, int L, int sum){
+    if(L>A.size()){
         if(sum==m) acc++;
         return;
     }
-    calDfs(A,size,L+1,sum+A[L-1]);
-    calDfs(A,size,L+1,sum-A[L-1]);
+    calDfs(A,L+1,sum+A[L-1]); //pstv
+    calDfs(A,L+1,sum-A[L-1]); //ngtv
 }
 void dfs(int L){
     if(L>n){
         vector<int> tmp;
         for(int i=1;i<=n;i++) if(ch[i]) tmp.push_back(a[i]);
-        calDfs(tmp, tmp.size(), 1, 0);        
+        calDfs(tmp, 1, 0);        
         return;
     }
     ch[L]=1;
-    dfs(L+1);
+    dfs(L+1); //참여
     ch[L]=0;
-    dfs(L+1);
+    dfs(L+1); //미참여
 }
 int main(){
     cin>>n>>m;
