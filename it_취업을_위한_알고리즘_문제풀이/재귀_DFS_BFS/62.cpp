8730af75e9ecc1e71adadb461c86a3276d9b7059
chjh6107 <chjh6107@gmail.com>
2021-01-31 17:57:39 +0900

    병합정렬

--- it_취업을_위한_알고리즘_문제풀이/재귀_DFS_BFS/62.cpp
+++ it_취업을_위한_알고리즘_문제풀이/재귀_DFS_BFS/62.cpp
@@ -1,34 +1,37 @@
 // 62. 병합정렬
 #include <iostream>
 using namespace std;
-int data[10], tmp[10];
+int tmp[10];
 
-void mergeSort(int left, int right){
+void merge(int *A, int left, int mid, int right){
+    int L = left;
+    int R = mid+1;
+    int K = left;
+
+    while(L<=mid && R<=right)                   // 정렬
+        tmp[K++] = A[L]<A[R]?A[L++]:A[R++];
+    while(L<=mid) tmp[K++]=A[L++];              // 나머지 값 처리
+    while(R<=right) tmp[K++]=A[R++];
+    for(int i=left;i<=right;i++)A[i]=tmp[i];    // 원래 배열에 복사
+    
+}
+
+void mergeSort(int *A, int left, int right){
     int mid;
-    int p1, p2, p3;
     if(left<right){
         mid=(left+right)/2;
-        mergeSort(left, mid);
-        mergeSort(mid+1, right);
-        p1=left;
-        p2=mid+1;
-        p3=left;
-        while(p1<=mid && p2<=right){
-            if(data[p1]<data[p2]) tmp[p3++]=data[p1++];
-            else tmp[p3++]=data[p2++];
-        }
-        while(p1<=mid) tmp[p3++]=data[p1++];
-        while(p2<=right) tmp[p3++]=data[p2++];
-        for(int i=left; i<=right; i++) data[i]=tmp[i];
+        mergeSort(A, left, mid);
+        mergeSort(A, mid+1, right);
+        merge(A,left,mid,right);
     }
 }
 
 int main(){
-    int n;
+    int n, data[10];
     cin>>n;
-    for(int i=1;i<=n;i++) cin>>data[i];
+    for(int i=0;i<n;i++) cin>>data[i];
 
-    mergeSort(1, n);
-    for(int i=1;i<=n;i++) cout<<data[i]<<" ";
+    mergeSort(data, 0, n-1);
+    for(int i=0;i<n;i++) cout<<data[i]<<" ";
 
 }
