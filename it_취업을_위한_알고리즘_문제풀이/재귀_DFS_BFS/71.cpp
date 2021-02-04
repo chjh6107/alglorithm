cd1581d7974c9e2dd544a8da5640e997a8ee1770
chjh6107 <chjh6107@gmail.com>
2021-02-04 22:22:38 +0900

    송아지 찾기(BFS : 상태트리탐색)

---
+++ it_취업을_위한_알고리즘_문제풀이/재귀_DFS_BFS/71.cpp
@@ -0,0 +1,31 @@
+// 71. 송아지 찾기(BFS : 상태트리탐색)
+#include <iostream>
+#include <queue>
+using namespace std;
+int S,E;
+int mv[3]={1,-1,5};
+int cnt[10001],ch[10001];
+queue<int> Q;
+void bfs(){
+    while(!Q.empty()){
+        int pos;
+        int curr = Q.front();
+        Q.pop();
+        if(curr==E) break;
+        for(int i=0;i<3;i++){
+            pos = curr+mv[i];
+            if(pos<0||pos>10000) continue;
+            if(!ch[pos]){
+                ch[pos]=1;
+                Q.push(mv[i]+curr);
+                cnt[pos]+=cnt[curr]+1;
+            }
+        }
+    }
+}
+int main(){
+    cin>>S>>E;
+    Q.push(S);
+    bfs();
+    cout<<cnt[E];
+}
