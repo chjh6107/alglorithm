d660bfa3ae7fa7ee942cc791d57c4a56bb03a767
chjh6107 <chjh6107@gmail.com>
2021-02-06 16:06:06 +0900

    최소힙(priority_queue : 우선순위 큐)

---
+++ it_취업을_위한_알고리즘_문제풀이/재귀_DFS_BFS/74.cpp
@@ -0,0 +1,18 @@
+// 74. 최소힙(priority_queue : 우선순위 큐)
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    priority_queue<int> pq;
+    int n;
+    while(1){
+        cin>>n;
+        if(n==-1) break;
+        if(n) pq.push(-n);
+        else{ //!n
+            cout<<(pq.empty()?-1:-pq.top())<<"\n";
+            pq.pop();
+        }
+    }
+}
