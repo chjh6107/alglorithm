5d1a20ec70cef9d10b32396f8bb5cd4e15eb46dd
chjh6107 <chjh6107@gmail.com>
2021-02-10 01:10:26 +0900

    숨바꼭질

---
+++ 분류/1697.cpp
@@ -0,0 +1,34 @@
+// 숨바꼭질
+// 그래프 이론, 그래프 탐색, 너비 우선 탐색
+#include <iostream>
+#include <queue>
+using namespace std;
+int visit[100001];
+queue<pair<int,int>> Q;
+int mv[]={-1,1};
+int n,k;
+void bfs(){
+    Q.push({n,0});
+    visit[n];
+    while(!Q.empty()){
+        int v=Q.front().first;
+        int time=Q.front().second;
+        Q.pop();
+        if(v==k){
+            cout<<time;
+            break;
+        }
+        for(int i=0;i<3;i++){
+            int next=v+(i<2?mv[i]:v);
+            if(next<0||next>100000)continue;
+            if(!visit[next]){
+                visit[next]=1;
+                Q.push({next,time+1});
+            }
+        }
+    }
+}
+int main(){
+    cin>>n>>k;
+    bfs();
+}
