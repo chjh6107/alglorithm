5850ce820781bea162e5eba89ecbf092949ee0f1
chjh6107 <chjh6107@gmail.com>
2021-02-09 19:46:41 +0900

    이항계수(메모이제이션)

---
+++ it_취업을_위한_알고리즘_문제풀이/그래프, DFS, BFS 관련 보충문제/76.cpp
@@ -0,0 +1,12 @@
+// 76. 이항계수(메모이제이션)
+#include <iostream>
+using namespace std;
+typedef long long ll;
+ll mem[21]={1};
+ll fact(ll x){mem[x]= x>1?x*fact(x-1):1;}
+int main(){
+    int N,R;
+    cin>>N>>R;
+    fact(20);
+    cout<<mem[N]/(mem[N-R]*mem[R]);
+}
