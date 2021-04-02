2dcdabf22e70de3803c4054dff45531ccf18b53f
chjh6107 <chjh6107@gmail.com>
2021-04-02 22:43:03 +0900

    다익스트라 알고리즘 pair버전

--- it_취업을_위한_알고리즘_문제풀이/그래프, DFS, BFS 관련 보충문제/80pair.cpp
+++ it_취업을_위한_알고리즘_문제풀이/그래프, DFS, BFS 관련 보충문제/80pair.cpp
@@ -6,16 +6,27 @@
 using namespace std;
 vector<pair<int, int>> graph[21];
 vector<int> dist(21,INF);
-priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq; //first 거리, second 정점
+priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq; //first ->cost, second ->cur
 void dijkstra(){
     dist[1]=0;
     pq.push({0,1});
     while(!pq.empty()){
-        int dist=pq.top().first;
-        int vertx=pq.top().second;
+        int cost=pq.top().first;
+        int cur=pq.top().second;
+        pq.pop();
+        if(dist[cur]<cost)continue;
+        for(int i=0;i<graph[cur].size();i++){
+            int next =graph[cur][i].first;
+            int nCost=graph[cur][i].second+cost;
+            if(dist[next]>nCost){
+                dist[next]=nCost;
+                pq.push({nCost,next});
+            }
+        }
     }
 }
 int main(){
+    freopen("input.txt","r",stdin);
     int N,M,a,b,c;
     cin>>N>>M;
     for(int i=0;i<M;i++){
@@ -23,4 +34,5 @@ int main(){
         graph[a].push_back({b,c});
     }
     dijkstra();
+    for(int i=2;i<=N;i++) cout<<N<<" : "<<(dist[i]==INF?"impossible":to_string(dist[i]))<<"\n";
 }
