324dac1b08603b9099fec043caf0e9f1b233c12a
chjh6107 <chjh6107@gmail.com>
2021-04-03 23:10:52 +0900

    최소비용 구하기

--- 분류/graph/1916.cpp
+++ 분류/graph/1916.cpp
@@ -3,40 +3,22 @@
 #include <vector>
 #include <queue>
 #define MAX 1001
-#define INF 100000000
+#define INF 2147000000
 using namespace std;
-vector<pair<int,int>> g[MAX];
-priority_queue<pair<int,int>>pq;
-void dijkstra(int x,vector<int> dist,int dst){
-    dist[x]=0;
-    pq.push({x,0});
-    while(!pq.empty()){
-        int now=pq.top().first;
-        int nowDist=-pq.top().second;
-        pq.pop();
-        if(nowDist>dist[now])continue;
-        for(int i=0;i<g[now].size();i++){
-            int next=g[now][i].first;
-            int nextDist=nowDist+g[now][i].second;
-            
-            if(dist[next]>nowDist){
-                dist[next]=nextDist;
-                pq.push({next,-nextDist});
-            }
-        }
-    }
-    cout<<dist[dst];
+
+vector<pair<int,int>> g[MAX]; // next,cost
+priority_queue<pair<int,int>> pq;
+vector<int> dist(MAX,INF);
+void dijkstra(){
+
 }
 int main(){
-
-    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    int N,M,src,dst,a,b,c;
+    ios_base::sync_with_stdio(0);cin.tie(NULL);
+    int N,M,a,b,c,src,dst;
     cin>>N>>M;
-    vector<int>dist(N+1,INF);
-    for(int i=0;i<M;i++){
+    while(M--){
         cin>>a>>b>>c;
         g[a].push_back({b,c});
     }
     cin>>src>>dst;
-    dijkstra(src,dist,dst);
 }
