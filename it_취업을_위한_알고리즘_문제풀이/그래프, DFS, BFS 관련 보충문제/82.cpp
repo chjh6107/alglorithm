2db46ac85f95d09ef7102098b1508738ff37045d
chjh6107 <chjh6107@gmail.com>
2021-04-07 19:03:09 +0900

    순열구하기(DFS : Depth First Search)

--- it_취업을_위한_알고리즘_문제풀이/그래프, DFS, BFS 관련 보충문제/82.cpp
+++ it_취업을_위한_알고리즘_문제풀이/그래프, DFS, BFS 관련 보충문제/82.cpp
@@ -1,28 +1,31 @@
-// 순열구하기(DFS : Depth First Search)
-#include <iostream>
+// 82. 순열구하기(DFS : Depth First Search)
+#include<iostream>
 using namespace std;
-int arr[15],res[15],visit[15];
-int N,R,acc=0;
+int nums[15];
+int visit[15];
+int res[15];
+int acc=0;
+int N,R;
 void dfs(int L){
     if(L==R){
-        for(int i=0;i<R;i++) cout<<res[i]<<" ";
-        cout<<'\n';
+        for(int i=0;i<R;i++)cout<<res[i]<<" ";
+        cout<<"\n";
         acc++;
         return;
     }
     for(int i=0;i<N;i++){
         if(!visit[i]){
-            res[L]=arr[i];
             visit[i]=1;
+            res[L]=nums[i];
             dfs(L+1);
             visit[i]=0;
         }
     }
 }
 int main(){
+    freopen("input.txt","r",stdin);
     cin>>N>>R;
-    for(int i=0;i<N;i++)
-        cin>>arr[i];   
+    for(int i=0;i<N;i++)cin>>nums[i];
     dfs(0);
     cout<<acc;
 }
