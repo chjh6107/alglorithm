48bad50e47d89a58037eaa0801b8f76be6f26039
chjh6107 <chjh6107@gmail.com>
2021-04-08 14:56:42 +0900

    수식만들기(삼성 SW역량평가 기출문제 : DFS활용)

---
+++ it_취업을_위한_알고리즘_문제풀이/그래프, DFS, BFS 관련 보충문제/85.cpp
@@ -0,0 +1,49 @@
+// 수식만들기(삼성 SW역량평가 기출문제 : DFS활용)
+#include<iostream>
+using namespace std;
+int N;
+int opCnt=0;
+int a[11];
+int op[4];
+int Min=2147000000, Max=0;
+void dfs(int L,int val){
+    // cout<<val<<"\n";
+    cout<<opCnt<<" "<<L<<"\n";
+    if(N==L){
+        if(Max<val)Max=val;
+        if(Min>val)Min=val;
+        return;
+    }
+    if(op[0]){
+        op[0]--;
+        dfs(L+1,val+a[L]);
+        op[0]++;
+    }
+    if(op[1]){
+        op[1]--;
+        dfs(L+1,val-a[L]);
+        op[1]++;
+    }
+    if(op[2]){
+        op[2]--;
+        dfs(L+1,val*a[L]);
+        op[2]++;
+    }
+    if(op[3]){
+        op[3]--;
+        dfs(L+1,val/a[L]);
+        op[3]++;
+    }
+
+}
+int main(){
+    freopen("input.txt","r",stdin);
+    cin>>N;
+    for(int i=0;i<N;i++)cin>>a[i];
+    for(int i=0;i<4;i++){
+        cin>>op[i];
+        opCnt+=op[i];
+    }
+    dfs(1,a[0]);
+    cout<<Min<<" "<<Max<<"\n";
+}
