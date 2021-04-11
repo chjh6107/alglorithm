bc2668713f43e5708fc9e844bee919fd42970d1a
chjh6107 <chjh6107@gmail.com>
2021-04-11 17:50:54 +0900

    섬나라 아일랜드(BFS 활용)

---
+++ it_취업을_위한_알고리즘_문제풀이/그래프, DFS, BFS 관련 보충문제/87.cpp
@@ -0,0 +1,17 @@
+// 섬나라 아일랜드(BFS 활용)
+#include<iostream>
+#include<queue>
+#define MAX 1000
+using namespace std;
+
+
+int graph[MAX][MAX];
+int visited[MAX];
+int dx[]={1,0,-1,0},dy[]={0,1,0,-1};
+void bfs(){
+
+}
+int main(){
+    freopen("input.txt","r",stdin);
+
+}
