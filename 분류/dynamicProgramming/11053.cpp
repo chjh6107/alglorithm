dc3ab0b62be9e8c9bfbabc1ff743e1febf445e1d
chjh6107 <chjh6107@gmail.com>
2021-04-24 19:42:10 +0900

    가장 긴 증가하는 부분 수열

---
+++ 분류/dynamicProgramming/11053.cpp
@@ -0,0 +1,18 @@
+// 가장 긴 증가하는 부분 수열
+// dp
+#include<iostream>
+using namespace std;
+int dp[1001]={0,1};
+int arr[1001];
+int main(){
+    int N,res=0;
+    cin>>N;
+    for(int i=1;i<=N;i++){
+        cin>>arr[i];
+        int max=0;
+        for(int j=1;j<=i;j++)
+            if(arr[i]>arr[j]&&dp[j]>max)max=dp[j];
+        if((dp[i]=++max)>res)res=dp[i];
+    }
+    cout<<res;
+}
