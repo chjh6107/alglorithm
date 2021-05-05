3feb09457c723f8e88b91eeb77080a2fd3439aa5
chjh6107 <chjh6107@gmail.com>
2021-05-05 23:42:11 +0900

    피자 배달 거리(삼성 SW역량평가 기출문제 : DFS활용)

--- it_취업을_위한_알고리즘_문제풀이/그래프, DFS, BFS 관련 보충문제/86.cpp
+++ it_취업을_위한_알고리즘_문제풀이/그래프, DFS, BFS 관련 보충문제/86.cpp
@@ -1,28 +1,35 @@
 // 86. 피자 배달 거리(삼성 SW역량평가 기출문제 : DFS활용)
 #include<bits/stdc++.h>
+#define LIM 2147000000
 using namespace std;
-int n,m;
+int n,m,res=LIM,dist,sum=0;
 vector<pair<int,int>>pz,hs;
-int ch[51];
-void dfs(int L,int cnt){
+int ch[20];
+void dfs(int L,int s){
     if(L==m){
-        for(int i=0;i<pz.size();i++){
-            if(ch[i])cout<<i<<" ";
+        sum=0;
+        for(int i=0;i<hs.size();i++){
+            int x1=hs[i].first;
+            int y1=hs[i].second;
+            dist=LIM;
+            for(int j=0;j<m;j++){
+                int x2=pz[ch[j]].first;
+                int y2=pz[ch[j]].second;
+                dist=min(dist,abs(x1-x2)+abs(y1-y2));
+            }
+            sum+=dist;
         }
-        cout<<"\n";
+        if(sum<res)res=sum;
         return;
     }
-    for(int i=0;i<pz.size();i++){
-        if(!ch[i]){
-            ch[i]=1;
-            dfs(L+1,cnt+1);
-            ch[i]=0;
-        }
+    for(int i=s;i<pz.size();i++){
+        ch[L]=i;
+        dfs(L+1,i+1);
     }
 }
 int main(){
     ios_base::sync_with_stdio(0);cin.tie(NULL);
-    freopen("input.txt","r",stdin);
+    // freopen("input.txt","r",stdin);
     int val;
     cin>>n>>m;
     for(int i=1;i<=n;i++){
@@ -33,4 +40,5 @@ int main(){
         }
     }
     dfs(0,0);
+    cout<<res;
 }
