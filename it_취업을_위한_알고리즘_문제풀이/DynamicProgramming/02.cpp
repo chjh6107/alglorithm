95a7d97964698b6918213cebbe99ca37b05eb352
chjh6107 <chjh6107@gmail.com>
2021-06-03 21:39:31 +0900

    네트워크 선 자르기(Top-Down : 재귀, 메모이제이션)

--- it_취업을_위한_알고리즘_문제풀이/DynamicProgramming/02.cpp
+++ it_취업을_위한_알고리즘_문제풀이/DynamicProgramming/02.cpp
@@ -1,14 +1,13 @@
 // 네트워크 선 자르기(Top-Down : 재귀, 메모이제이션)
 #include<iostream>
-int dp[46];
+int d[46];
 int dfs(int n){
-    if(n==1||n==2)return dp[n]=n;
-    if(dp[n]) return dp[n];
-    return dp[n]=dfs(n-1)+dfs(n-2);
+    if(n<=2) return d[n]=n;
+    return d[n]=dfs(n-1)+dfs(n-2);
 }
 int main(){
-    int N;
-    std::cin>>N;
-    dfs(N);
-    std::cout<<dfs(N);
+    int n;
+    scanf("%d",&n);
+    dfs(n);
+    printf("%d",d[n]);
 }
