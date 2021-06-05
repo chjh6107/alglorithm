1f2dbed80a3ef885b292e6dd750d47daac0b2956
chjh6107 <chjh6107@gmail.com>
2021-06-05 22:29:44 +0900

    최대 선 연결하기

---
+++ it_취업을_위한_알고리즘_문제풀이/DynamicProgramming/06.cpp
@@ -0,0 +1,10 @@
+// 최대 선 연결하기
+#include<iostream>
+using namespace std;
+int main(){
+    freopen("input.txt","r",stdin);
+    int N;
+    int arr[100];
+    cin>>N;
+    for(int i=0;i<N;i++)cin>>arr[i];
+}
